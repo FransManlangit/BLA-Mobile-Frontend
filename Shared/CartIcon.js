ae9304d8b718581c5cbd0817764d107e329eac9c
Mcheaven <115719615+FransManlangit@users.noreply.github.com>
2024-04-16 01:17:28 +0800

    Installed npx expo run:android

--- Shared/CartIcon.js
+++ Shared/CartIcon.js
@@ -1,46 +1,46 @@
-import React from "react";
-import { StyleSheet, View } from "react-native";
-import { Badge, Text, VStack } from "native-base";
-import { useSelector, } from 'react-redux'
-import { DocumentPlusIcon } from "react-native-heroicons/solid";
-import { COLORS } from "../assets/constants"
+  import React from "react";
+  import { StyleSheet, View } from "react-native";
+  import { Badge, Text, VStack } from "native-base";
+  import { useSelector, } from 'react-redux'
+  import { DocumentPlusIcon } from "react-native-heroicons/solid";
+  import { COLORS } from "../assets/constants"
 
 
-const CartIcon = (props) => {
+  const CartIcon = (props) => {
 
-  const color = props.color
+    const color = props.color
 
-  const cartItems = useSelector(state => state.cartItems);
+    const cartItems = useSelector(state => state.cartItems);
 
-  return (
-    <>
-      {cartItems.length > 0 ? (
-        <VStack space={2} alignItems="center">
-          <View className="p-5">
-        <Badge 
-        className="bg-[#B1A079] border-solid border-white"
-        rounded="md"
-        mb={-4}
-        mr={-4}
-        position="absolute"
-        zIndex={1}
-        variant="solid"
-        alignSelf="flex-end"
-        _text={{
-          fontSize: 12,
-          color: COLORS.white
-        }}
-        >
-         {cartItems.length}
-        </Badge>
-        <DocumentPlusIcon color={color}/>
-        </View>
-        </VStack>
-      ) : (
-        <DocumentPlusIcon color={color}/>
-      )}
-    </>
-  );
-};
-  
-export default CartIcon
+    return (
+      <>
+        {cartItems.length > 0 ? (
+          <VStack space={2} alignItems="center">
+            <View className="p-5">
+          <Badge 
+          className="bg-[#B1A079] border-solid border-white"
+          rounded="md"
+          mb={-4}
+          mr={-4}
+          position="absolute"
+          zIndex={1}
+          variant="solid"
+          alignSelf="flex-end"
+          _text={{
+            fontSize: 12,
+            color: COLORS.white
+          }}
+          >
+          {cartItems.length}
+          </Badge>
+          <DocumentPlusIcon color={color}/>
+          </View>
+          </VStack>
+        ) : (
+          <DocumentPlusIcon color={color}/>
+        )}
+      </>
+    );
+  };
+    
+  export default CartIcon
